d31b8883b1712cf55cb29de9f36ab988e1a492da
w01verine <56714639+w01verine@users.noreply.github.com>
2023-06-11 02:19:44 -0400

    Create CommonPacketTypeAttackMerthod.py

---
+++ CommonPacketTypeAttackMerthod.py
@@ -0,0 +1,21 @@
+# Created By: Tommie Navitskas 2022
+
+# The following displays the attack method based on the provided protocol
+
+def attackMethod(protocol):
+    if protocol in ['HTTP', 'FTP', 'SMTP', 'DNS']:
+        print('Exploit')
+    elif protocol in ['RPC', 'SIP', 'RTP']:
+        print('Phishing')
+    elif protocol in ['SSL', 'SNMP', 'XMPP']:
+        print('Hijacking')
+    elif protocol in ['TCP', 'UDP', 'SCTP', 'DCCP']:
+        print('Reconnaissance')
+    elif protocol in ['IPv4', 'IPv6', 'ICMP', 'IGMP']:
+        print('Man In The Middle')
+    elif protocol in ['Ethernet II', 'IEEE 802.1Q', 'PPP', 'HDLC', 'ARP']:
+        print('Spoofing')
+    elif protocol in ['IEEE 802.3', 'IEEE 802.11', 'IEEE 802.15.1', 'IEEE 802.15.4']:
+        print('Sniffing')
+    else:
+        print('Protocol not found')
